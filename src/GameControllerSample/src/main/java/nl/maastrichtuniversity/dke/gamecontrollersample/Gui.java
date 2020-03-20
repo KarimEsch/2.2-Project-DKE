5b3e26b92f14467596ddb73552d1b299ac1fe5c8
KarimEsch <45858208+KarimEsch@users.noreply.github.com>
2020-03-20 19:19:23 +0100

    Update Gui.java
    Adding wall and spawn area guard to the GUI

--- src/GameControllerSample/src/main/java/nl/maastrichtuniversity/dke/gamecontrollersample/Gui.java
+++ src/GameControllerSample/src/main/java/nl/maastrichtuniversity/dke/gamecontrollersample/Gui.java
@@ -3,8 +3,12 @@ package GameControllerSample.src.main.java.nl.maastrichtuniversity.dke.gamecontr
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.stage.Screen;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import java.util.ArrayList;
+
+import static javafx.scene.paint.Color.BLACK;
+import static javafx.scene.paint.Color.BLUE;
 
 public class Gui extends Application{
     protected static Scenario scenario;
@@ -15,8 +19,33 @@ public class Gui extends Application{
     }
 
     public void start(Stage primaryStage) throws Exception {
+        //Adding the walls to the GUI
+        ArrayList<Area> walls = scenario.getWalls ();
+        //Adding the spawn area guard added to the GUI
+        Rectangle SpawnAreaG = new Rectangle(  );
+
+        SpawnAreaG.setX(scenario.spawnAreaGuards.leftBoundary);
+        SpawnAreaG.setY(scenario.spawnAreaGuards.bottomBoundary);
+        SpawnAreaG.setWidth(scenario.spawnAreaGuards.rightBoundary - scenario.spawnAreaGuards.leftBoundary);
+        SpawnAreaG.setHeight ( scenario.spawnAreaGuards.topBoundary - scenario.spawnAreaGuards.bottomBoundary );
+
         primaryStage.setTitle("Team 4 -  Project 2.2");
         Group root = new Group();
+        Group allWalls = new Group();
+        for (int i=0; i<walls.size () ; i++) {
+            System.out.println ( walls.get ( i ).bottomBoundary );
+            Rectangle rectangle = new Rectangle ();
+            rectangle.setX(walls.get ( i ).leftBoundary);
+            rectangle.setY(walls.get ( i ).bottomBoundary);
+            rectangle.setWidth(walls.get ( i ).rightBoundary - walls.get ( i ).leftBoundary);
+            rectangle.setHeight(walls.get ( i ).topBoundary - walls.get ( i ).bottomBoundary);
+            rectangle.setArcHeight ( 5 );
+            rectangle.setFill ( BLUE );
+            rectangle.setStroke ( BLACK );
+            allWalls.getChildren ().add ( rectangle);
+        }
+        root.getChildren ().add(allWalls);
+        root.getChildren ().add(SpawnAreaG );
         Scene scene = new Scene(root, scenario.mapWidth, scenario.mapHeight);
         primaryStage.setScene(scene);
         primaryStage.show();
